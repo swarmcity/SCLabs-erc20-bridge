52dd0fa2deb17b90833260ced73f7c133a88e8e1
Stefaan Ponnet <stefaan@ponnet.com>
2018-02-02 18:26:49 +0100

    foreignBridge now implements the ITokenRecepient - as specified in ERC777

--- contracts/ForeignERC777Bridge.sol
+++ contracts/ForeignERC777Bridge.sol
@@ -1,15 +1,18 @@
 pragma solidity ^0.4.18;
 
 import 'eip777/contracts/ReferenceToken.sol';
+import 'eip777/contracts/ITokenRecipient.sol';
 import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 
-contract ForeignERC777Bridge is Ownable {
+contract ForeignERC777Bridge is Ownable, ITokenRecipient {
 
 	//track used fillup hashes
 	mapping(bytes32=>bool) withdrawRequests;
 
 	// maps home token addresses -> foreign token addresses
 	mapping(address=>address) tokenMap;
+
+	event WithdrawRequest(address _to,uint256 _amount,bytes32 _withdrawhash);
 
 	function registerToken(address _homeAddress,string _name, string _symbol) public onlyOwner {
 		assert(tokenMap[_homeAddress] == 0);
@@ -18,13 +21,28 @@ contract ForeignERC777Bridge is Ownable {
 		tokenMap[_homeAddress] = t;
 	}
 
-	function mintTokens(address _token, address _recepient,uint256 _amount) public onlyOwner {
+	function mintTokens(address _token, address _recipient,uint256 _amount) public onlyOwner {
 		assert(tokenMap[_token] != 0);
-		ReferenceToken(tokenMap[_token]).mint(_recepient,_amount,"");
+		ReferenceToken(tokenMap[_token]).mint(_recipient,_amount,"");
 	}
 
-	// function withDrawRequest(address _from, address _token,address _recepient,uint256 _amount) public onlyOwner {
-	// 	//assert(!withdrawRequests[_id]);
+	// the ERC777 token will call this function when a token is sent to the bridge.
+	function tokensReceived(
+	    address from,
+	    address to,
+	    uint amount,
+	    bytes userData,
+	    address operator,
+	    bytes operatorData
+	) public{
+		bytes32 hash = sha256(from,amount,userData);
+		// notify validators of this request.
+		WithdrawRequest(from,amount,hash);
+	}
+
+
+	// function withDrawRequest(address _token,address _recipient,uint256 _amount) public onlyOwner {
+	// assert(!withdrawRequests[_id]);
 	// }
 }
 
--- contracts/HomeERC20Bridge.sol
+++ contracts/HomeERC20Bridge.sol
@@ -1,20 +1,31 @@
 pragma solidity ^0.4.18;
 
 import 'zeppelin-solidity/contracts/token/ERC20/ERC20Basic.sol';
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 
-contract HomeERC20Bridge {
+contract HomeERC20Bridge is Ownable {
 
-	// Event created on validator gets added deposit.
+	// Event created on validator gets added
 	event ValidatorAdded (address validator);
+	event ValidatorRemoved (address validator);
 
-	//address[] _validators;
+	mapping (address=>bool) validators;
 
-	// function HomeERC20Bridge(address[] _validators){
-	// 	validators = _validators;
-	// }
-
-	function withdraw(address _token, address _recepient,uint _amount) public{
+	function withdraw(address _token, address _recepient,uint _amount,bytes32 _withdrawhash, uint8[] _validators,uint8[] v, bytes32[] r, bytes32[] s) public{
+		// iterate over validators
+		// check if signature 
 		assert(ERC20Basic(_token).transfer(_recepient,_amount));
+	}
+
+	function addValidator(address _validator) public onlyOwner {
+		assert(validators[_validator] != true);
+		validators[_validator] = true;
+		ValidatorAdded(_validator);
+	}
+
+	function removeValidator(address _validator) public onlyOwner{
+		validators[_validator] = false;
+		ValidatorRemoved(_validator);
 	}
 }
 
